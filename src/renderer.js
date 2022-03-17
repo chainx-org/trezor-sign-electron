e59034702157d76df4cc4ef51c88eb5ea4c73fc8
qinghuan-chain <1245816264@qq.com>
2022-03-17 15:48:08 +0800

    feat: add tocold number

--- src/renderer.js
+++ src/renderer.js
@@ -13,7 +13,23 @@ const textRawTx = document.getElementById('text-rawtx');
 
 const btnSignToCold = document.getElementById('sign-to-cold');
 const textToCold = document.getElementById('text-tocold');
+const inputBitcoinNumber = document.getElementById('input-bitcoin-number');
+const inputBitcoinFee = document.getElementById('input-bitcoin-fee');
+inputBitcoinFee.value = 29422
+inputBitcoinNumber.value = 5.47
+
 const trezor = new Trezor();
+
+btnSignToCold.onclick = async () => {
+    await trezor.init()
+    let toColdNumber = inputBitcoinNumber.value
+    let toColdFee = inputBitcoinFee.value
+    console.log(`toColdNumber: ${toColdNumber} toColdFee: ${toColdFee}`);
+    const hash = await contructToCold(toColdNumber,toColdFee)
+    console.log(`hash: ${hash}`);
+    textRawTx.value = hash
+    window.alert(JSON.stringify(hash))
+}
 
 btnSignWithTrezor.onclick = async () => {
    await trezor.init()
@@ -29,12 +45,4 @@ btnSignWithTrezor.onclick = async () => {
     console.log(`signData: ${JSON.stringify(signData)}`);
 };
 
-btnSignToCold.onclick = async () => {
-    await trezor.init()
-    let toColdNumber = textToCold.value
-    const hash = await contructToCold(23245,toColdNumber)
-    console.log(`hash: ${hash}`);
-    textRawTx.value = hash
-    window.alert(JSON.stringify(hash))
-}
 
