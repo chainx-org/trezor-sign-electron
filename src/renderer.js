555727f669f54e7d64d64fccf80d3cbb4f903c41
qinghuan-chain <1245816264@qq.com>
2022-03-17 16:08:07 +0800

    feat: fix bug

--- src/renderer.js
+++ src/renderer.js
@@ -15,7 +15,7 @@ const btnSignToCold = document.getElementById('sign-to-cold');
 const inputBitcoinNumber = document.getElementById('input-bitcoin-number');
 const inputBitcoinFee = document.getElementById('input-bitcoin-fee');
 
-inputBitcoinNumber.value = 4.51664604;
+inputBitcoinNumber.value = 4.5;
 inputBitcoinFee.value = BITCOIN_FEE_RATE
 const trezor = new Trezor();
 
