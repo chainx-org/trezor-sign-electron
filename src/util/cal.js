1f06e70ace13a24b2aa823f3a0530c3b5eea6e6e
qinghuan-chain <1245816264@qq.com>
2022-03-29 10:43:22 +0800

    feat: add defalut amount

--- src/util/cal.js
+++ src/util/cal.js
@@ -1,28 +1,31 @@
-const { getUnspents, calcTargetUnspents } = require("../bitcoin/bitcoin.js")
+const { getUnspents } = require("../bitcoin/bitcoin.js")
 const Api = require("../chainx")
 
 // 根据utxo 数量计算 amount
 async function calNeedUtxo(count){
     const bitcoinType = "mainnet";
-    const info = await Api.getInstance().getTrusteeSessionInfo(0);
-    const hotAddr = String(info.hotAddress.addr);
-    const unspents = await getUnspents(hotAddr, bitcoinType);
-    unspents.sort((a, b) => {
+    try {
+        const info = await Api.getInstance().getTrusteeSessionInfo(0);
+        const hotAddr = String(info.hotAddress.addr);
+        const unspents = await getUnspents(hotAddr, bitcoinType);
+        unspents.sort((a, b) => {
             return b.amount > a.amount
-    });
-    const unspentsLimit = []
-    let utxoCalamount = 0;
-    console.log(`utxo ${JSON.stringify(unspents[0])}`)
-
-    // 如果 count 大于 当前utxo 数量，则使用当前 utxo， 否则使用 count 个 utxo
-    const needCount = unspents.length < count ? unspents.length : count;
-
-    for (let i = 0; i < needCount; i ++) {
-        unspentsLimit.push(unspents[i])
-        utxoCalamount += unspents[i].amount
-    }
+        });
+        const unspentsLimit = []
+        let utxoCalamount = 1;
+        console.log(`utxo ${JSON.stringify(unspents[0])}`)
+        // 如果 count 大于 当前utxo 数量，则使用当前 utxo， 否则使用 count 个 utxo
+        const needCount = unspents.length < count ? unspents.length : count;
+        for (let i = 0; i < needCount; i ++) {
+            unspentsLimit.push(unspents[i])
+            utxoCalamount += unspents[i].amount
+        }
+        utxoCalamount / Math.pow(10, 8)
+    } catch (error) {
+        console.log(e)
+    } 
     // 返回count个utxto的总金额
-    return utxoCalamount / Math.pow(10, 8)
+    return utxoCalamount
 }
 
 module.exports = {
